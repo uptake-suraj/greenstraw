fbc66a6337fee738651ade10cdc1ab91ccb0dcb0
siddharth <siddharth@stackss.tech>
2025-05-02 15:48:50 +0530

    test scroll

--- src/components/home/HorizontalScrollCards.jsx
+++ src/components/home/HorizontalScrollCards.jsx
@@ -1,15 +1,42 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import {horizontalData} from "../../assets/data/horizontalData";
-import { useHorizontalScroll } from "../../hook/useHorizontalScroll";
+// import { useHorizontalScroll } from "../../hook/useHorizontalScroll";
+import gsap from "gsap";
 import HorizontalCard from "../card/HorizontalCard";
 
 const HorizontalScrollCards = () => {
-  const scrollRef = useHorizontalScroll();
+  // const scrollRef = useHorizontalScroll();
+  const sectionRef = useRef(null);
+  const triggerRef = useRef(null);
+
+  useEffect(() => {
+    const pin = gsap.fromTo(
+      sectionRef.current,
+      {
+        translateX: 0,
+      },
+      {
+        translateX: "-300vw",
+        ease: "none",
+        duration: 1,
+        scrollTrigger: {
+          trigger: triggerRef.current,
+          start: "top top",
+          end: "2000 top",
+          scrub: 0.6,
+          pin: true,
+        },
+      }
+    );
+    return () => {
+      pin.kill();
+    };
+  }, []);
 
   return (
     <section className="lg:my-20">
-      <div className="w-full">
-        <div ref={scrollRef} className="flex gap-8 w-max px-4 py-20">
+      <div className="w-full" ref={triggerRef}>
+        <div ref={sectionRef} className="flex gap-8 w-max px-4 py-20">
           {horizontalData?.map((item , index) => (
             <HorizontalCard key={item.id} {...item} pageNumber={index + 1} totalPages={horizontalData.length} />
           ))}
